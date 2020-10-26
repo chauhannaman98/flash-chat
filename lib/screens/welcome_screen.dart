c0d6e7ae3cff5403843a65c42f2188b47c5a214a
Naman Chauhan <chauhannaman98@gmail.com>
2020-10-26 22:55:31 +0530

    Animated background animation - tween

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -13,6 +13,7 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
 
   AnimationController controller;
+  Animation animation;
 
   @override
   void initState() {
@@ -22,16 +23,30 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
       duration: Duration(seconds: 1),
       vsync: this,
     );
+
+    animation = ColorTween(
+      begin: Colors.blueGrey,
+      end: Colors.white,
+    ).animate(controller);
+
     controller.forward();
+
     controller.addListener(() {
-      print(controller.value);
+      setState(() {});
+      print(animation.value);
     });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
