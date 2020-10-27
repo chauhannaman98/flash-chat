7e5689674ae62b2274861b6b4ed35698249f7c4b
Naman Chauhan <chauhannaman98@gmail.com>
2020-10-27 14:49:42 +0530

    Added typewriter animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
 
@@ -33,7 +34,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
 
     controller.addListener(() {
       setState(() {});
-      print(animation.value);
     });
   }
 
@@ -62,11 +62,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
                     height: 60.0,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  speed: Duration(milliseconds: 500),
+                  textStyle: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
+                    color: Colors.black,
                   ),
                 ),
               ],
